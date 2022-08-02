282a9a84a0c3502658a8514f07053653063eab0a
Filip Bochniak <bochniakf@ela.local>
2022-08-02 11:56:36 +0200

    Added if statement in lib_led.c

--- playground/LIB_LED/lib_led.c
+++ playground/LIB_LED/lib_led.c
@@ -6,27 +6,30 @@ static LED_STEPS_E_T s_CurrentStep = STEP_NO_LED_E;
 static _Bool s_TimerElapsed = false;
 
 void ledTask(void){
-	switch(s_CurrentStep){
-			  	  case STEP_NO_LED_E:
-			  		  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_SET);
-			  		  HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_SET);
-			  		  break;
-			  	  case STEP_RED_LED_E:
-			  		  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
-			  		  HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_SET);
-			  		  break;
-			  	  case STEP_GREEN_LED_E:
-			  		  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_SET);
-			  		  HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_RESET);
-			  		  break;
-			  	  case STEP_BOTH_LED_E:
-			  		  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
-			  		  HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_RESET);
-			  		  break;
-			  	  default:
-			  		  /* do nothing */
-			  		  break;
-			  }
+	if(true == s_TimerElapsed){
+		switch(s_CurrentStep){
+					  case STEP_NO_LED_E:
+						  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_SET);
+						  HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_SET);
+						  break;
+					  case STEP_RED_LED_E:
+						  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
+						  HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_SET);
+						  break;
+					  case STEP_GREEN_LED_E:
+						  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_SET);
+						  HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_RESET);
+						  break;
+					  case STEP_BOTH_LED_E:
+						  HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
+						  HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_RESET);
+						  break;
+					  default:
+						  /* do nothing */
+						  break;
+				  }
+		s_TimerElapsed = false;
+	}
 }
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
